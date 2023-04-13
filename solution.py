e46169bf76950358d4f03e51e817c71e17222f8a
Egor Chernov <90514666+EgorChernov5@users.noreply.github.com>
2023-04-13 11:40:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,7 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    alpfa = 0.02
+    alpfa = 0.05
     p1 = x_success / x_cnt
     p2 = y_success / y_cnt
     p = (x_success + y_success) / (x_cnt + y_cnt)
